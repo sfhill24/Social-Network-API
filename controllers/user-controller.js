122fec4dc6053d3024317b5f651e0ee4186c930a
Shelly Foxworth-Hill <sfhill24@gmail.com>
2022-12-07 11:44:51 -0500

    code clean up

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -66,27 +66,20 @@ const userController = {
             .catch(err => res.status(400).json(err));
     },
 
-    //DELETE user and thoughts by its id (deleteUser)
-    deleteUser({ params }, res) {
-        User.findOne({ _id: params.userId })
-            .populate({
-                path: 'thoughts',
-                select: '-_v'
-            })
-            .then((dbUserData) => {
 
-                if (!dbUserData) {
-                    res.status(404).json({ message: 'No user found with this id!' });
-                    return;
-                }
-
-                Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
-                User.deleteOne({ _id: params.userId })
-
-                res.json(dbUserData);
-            })
-            .catch(err => res.status(400).json(err));
+    //DELETE user (deleteUser)
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.userId})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({message: "No user found with this id!"});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res. status(400).json(err));
     },
+
 
     //POST a new friend to a user's friend list (addFriend)
     addFriend({ params }, res) {
@@ -123,9 +116,6 @@ const userController = {
             })
             .catch(err => res.status(400).json(err));
     }
-
-    //BONUS: DELETE user's associated thoughts when deleted
-
 };
 
 module.exports = userController;
